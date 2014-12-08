7595c657de71ed5d08b4808e9888848bf1027156
Chimaobi Jibuike <jibuikec@uni.coventry.ac.uk>
2014-12-08 11:30:40 +0000

    Import of financial position API

--- API_IMPORT.py
+++ API_IMPORT.py
@@ -6,3 +6,5 @@ url = "http://dev.c0l.in:5984/financial_positions/30e901a7b7d8e98328dcd77c3600fa
 response = urllib2.urlopen(url).read()
 data = json.loads(response.decode('utf8'))
 print data
+
+######12/09/1995 CJ######
