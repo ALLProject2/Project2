215d3b35748c881fbfa9498b09c0310abf08c021
Chimaobi Jibuike <jibuikec@uni.coventry.ac.uk>
2014-12-08 12:31:24 +0000

    Revert "Import of financial position API"
    This reverts commit 3646935853eb0b9ba13332b0a67d29cd06fa7965.

--- API_IMPORT.py
+++ API_IMPORT.py
@@ -5,25 +5,6 @@ url = "http://dev.c0l.in:5984/financial_positions/30e901a7b7d8e98328dcd77c3600fa
 
 response = urllib2.urlopen(url).read()
 data = json.loads(response.decode('utf8'))
-#print data
+print data
 
-#sort data into items
-for key, value in data.iteritems():
-    print key, value
-
-#define non_current variable
-non_current = float(data['company']['non_current_liabilities'])
-
-#print non current liabilities
-print ("Non current liabilities"), non_current
-
-
-#Total Assets = Non current Assets + Current Assets
-
-Assets_non_current = float(data['company']['non_current_assets'])
-
-Assets_current = float(data['company']['current_assets'])
-
-totalAssets = Assets_non_current + Assets_current
-
-print ("Total assets are"), totalAssets
+######12/09/1995 CJ######
