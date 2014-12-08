ce800d07f20f1628f99f05b8d52c6127480c2343
Simran Kundra <sim_kundra@hotmail.co.uk>
2014-12-08 10:06:44 +0000

    Import of financial position API
    Here is framework for import the financial statement using web API

---
+++ API_IMPORT.py
@@ -0,0 +1,8 @@
+import urllib2
+import json
+
+url = "http://dev.c0l.in:5984/financial_positions/30e901a7b7d8e98328dcd77c3600fa43"
+
+response = urllib2.urlopen(url).read()
+data = json.loads(response.decode('utf8'))
+print data
