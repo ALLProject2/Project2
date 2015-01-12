2107c82c2a5e493fbc855c7514d77bf87ab3a835
Callum A Horton <callumhorton@virginmedia.com>
2015-01-12 14:31:14 +0000

    Support Function

--- Support_Menu.py
+++ Support_Menu.py
@@ -1,11 +1,11 @@
 #Support
 
-print ("SCCJB Customer Support ")
-print ("")
-print ("Email: SCCJB@Support.co.uk")
-print ("")
-print ("Phone: 024 0612 1224")
-print ("")
-print ("FAQs")
-print ("")
-print ("Where is the CSV file I exported?")
+def Support:
+    print ("SCCJB Customer Support ")
+    print ("")
+    print ("Email: SCCJB@Support.co.uk")
+    print ("")
+    print ("Phone: 024 0612 1224")
+    print ("")
+    print ("Open: 9am - 5pm")
+    print ("Open: Monday - Saturday")
