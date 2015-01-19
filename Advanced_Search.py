88d1883f9f063443e3e7a5739fa0c96de2f5a57a
Simran Kundra <sim_kundra@hotmail.co.uk>
2015-01-19 14:55:10 +0000

    advanced search

---
+++ Advanced_Search.py
@@ -0,0 +1,10 @@
+str(myNumbers = [30e901a7b7d8e98328dcd77c369b6ad7, 30e901a7b7d8e98328dcd77c369b717f, 30e901a7b7d8e98328dcd77c369b7cde, 30e901a7b7d8e98328dcd77c369b86b5, 30e901a7b7d8e98328dcd77c369b8ed6])
+#Define array
+
+
+flag == 0
+
+while flag == 0:
+    myNumbers[0]
+    print myNumbers[0]
+    
--- ListBuild.py
+++ ListBuild.py
@@ -1,15 +1,22 @@
 import urllib2
 import json
 
-FPurl = "http://dev.c01.in:5984/financial_positions/_all_docs"
+FPurl = "http://dev.c0l.in:5984/income_statements/_all_docs"
 response = urllib2.urlopen(FPurl).read()
 data= json.loads(response)
 
 for item in data['rows']:
     print item['key']
+
+    import urllib2
+    import json
+
+    FPurl2 = "http://dev.c0l.in:5984/income_statements/"
+    response2 = urllib2.urlopen(FPurl2+item['key']).read()
+    data2= json.loads(response)
+    print data2
     
-    FPurl = "http://dev.c01.in:5984/financial_positions/" + item['key']
-    response = urllib2.urlopen(FPurl).read()
-    data= json.loads(response)
 
     
+
+
