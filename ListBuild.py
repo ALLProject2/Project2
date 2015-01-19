7be557a6fce727ffc69798e186392328e6d3c73c
Simran Kundra <sim_kundra@hotmail.co.uk>
2015-01-19 15:34:53 +0000

    IT WORKS!

--- ListBuild.py
+++ ListBuild.py
@@ -3,20 +3,13 @@ import json
 
 FPurl = "http://dev.c0l.in:5984/income_statements/_all_docs"
 response = urllib2.urlopen(FPurl).read()
-data= json.loads(response)
+data = json.loads(response)
 
 for item in data['rows']:
-    print item['key']
-
-    import urllib2
-    import json
-
-    FPurl2 = "http://dev.c0l.in:5984/income_statements/"
-    response2 = urllib2.urlopen(FPurl2+item['key']).read()
-    data2= json.loads(response)
-    print data2
-    
-
+    FPurl2 = "http://dev.c0l.in:5984/income_statements/" + item['id']
+    response2 = urllib2.urlopen(FPurl2).read()
+    data2 = json.loads(response2)
+    print data2['company']['name']
     
 
 
